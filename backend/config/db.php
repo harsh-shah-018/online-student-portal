de992a22c99d01b7e95f5dd653f1eedc0dec9a6b
harsh-shah123 <harshnshah.shivinfotech@gmail.com>
2026-04-27 01:02:42 +0530

    fixes

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,7 +1,10 @@
 <?php
 // backend/config/db.php
 
-// CORS is handled globally by .htaccess
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
